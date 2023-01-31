4a306ac9d256a2110342a34020a1d82a01cba7fe
marc.sedo@clearpeaks.com <marc.sedo@clearpeaks.com>
2023-01-31 11:53:22 +0000

    seq

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,12 +1,21 @@
-select
+SELECT 
+    seq_cust.nextval as customer_wid,
+    A.CUSTOMER_NAME,
+    A.phone,
+    A.address_line,
+    A.postal_code,
+    A.contact_name,
+    current_timestamp() as CREATED_DATE
+   FROM 
+(select
 
-    row_number() over (order by customer_name) as customer_id,
-    customer_name,
+
+    customer_name AS CUSTOMER_NAME,
     phone,
     address_line,
     postal_code,
     contact_name
 
-from {{ ref("stg_orders") }}
+from {{ref('stg_orders')}}
 
-group by customer_name, phone, address_line, postal_code, contact_name
+group by customer_name, phone, address_line, postal_code, contact_name) A
--- models/marts/fact_orders.sql
+++ models/marts/fact_orders.sql
@@ -3,7 +3,7 @@ select
     Order_Number,
     Order_Line_Number,
     prod.Product_Code,
-    cust.Customer_ID,
+    cust.customer_wid,
     geo.City_ID,
     Quantity_Ordered,
     Price_Each,
